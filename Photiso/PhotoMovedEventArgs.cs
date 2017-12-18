703d2ab8ff6e24ad78a84b5ac5635f8c24acec7c
BellaCode <geoff.cox@live.com>
2017-12-18 14:13:00 -0800

    Put project into public repository

---
+++ Photiso/PhotoMovedEventArgs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Photiso
+{
+    public class PhotoMovedEventArgs : EventArgs
+    {
+        private readonly string _sourceFileName;
+        private readonly string _destinationFileName;
+
+        public PhotoMovedEventArgs(string sourceFileName, string destinationFileName)
+        {
+            if (sourceFileName == null)
+            {
+                throw new ArgumentNullException("sourceFileName");
+            }
+            if (destinationFileName == null)
+            {
+                throw new ArgumentNullException("destinationFileName");
+            }
+
+            this._sourceFileName = sourceFileName;
+            this._destinationFileName = destinationFileName;            
+        }       
+
+        public string SourceFileName
+        {
+            get
+            {
+                return this._sourceFileName;
+            }
+        }
+
+        public string DestinationFileName
+        {
+            get
+            {
+                return this._destinationFileName;
+            }
+        }        
+    }
+}
