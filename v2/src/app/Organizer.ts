2c3c7e48180a102707d0d39089658c714afc8ffa
GeoffCox <geoff.cox@live.com>
2018-11-07 00:03:13 -0800

    Update v2/src/app/Organizer.ts

--- v2/src/app/Organizer.ts
+++ v2/src/app/Organizer.ts
@@ -2,9 +2,7 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import * as exif from "fast-exif";
-import { getHeapSpaceStatistics } from "v8";
-import { strictEqual } from "assert";
-import { stringify } from "querystring";
+import * as crypto from "crypto";
 
 export type Organizer = {
     organize(): Promise<void>;
@@ -12,7 +10,19 @@ export type Organizer = {
 
 export const createOrganizer = (unorganizedDir: string, organizedDir: string, duplicatesDir: string) => {
 
-    const getPhotoTakenTime = async (file: string, stats: fs.Stats): Promise<Date> => {
+    const padStart = (value: number, length: number): string => {
+        let text = value.toString();
+        return `${'0'.repeat(length - text.length)}${text}`;
+    }
+
+    const photoExts = ['.JPG', '.JPEG', '.PNG', '.BMP', '.TIF', '.WMP', '.ICO']
+
+    const isPhotoFile = (file: string) => {
+        const ext = path.parse(file).ext.toUpperCase();
+        return photoExts.some(e => e === ext);
+    }
+
+    const getPhotoTakenTime = async (file: string): Promise<Date> => {
 
         try {
             const exifData = await exif.read(file);
@@ -27,38 +37,68 @@ export const createOrganizer = (unorganizedDir: string, organizedDir: string, du
             console.warn(error);
         }
 
+        const stats = await fs.stat(file);
         const { ctime, mtime } = stats;
         return ctime < mtime ? ctime : mtime;
     }
 
-    const twoDigitPad = (value: number): string => {
-        return value < 10 ? `0${value}` : `${value}`;
+    const getPhotoContentHash = async (file: string) : Promise<string> => {                
+        return new Promise<string>((resolve, reject) => {
+            const hash = crypto.createHash('sha256');
+        const input = fs.createReadStream(file);
+        
+            input.on('readable', () => {
+                const data = input.read();
+                if (data)
+                hash.update(data);
+                else {
+                    resolve(`${hash.digest('hex')}`);
+                }
+            });
+        });
     }
 
-    const getPhotoDestDir = async (file: string, stats: fs.Stats): Promise<string> => {
-        const takenTime = await getPhotoTakenTime(file, stats);
+    const getPhotoDestFile = async (sourceFile: string) : Promise<string> => {
 
-        const year = takenTime.getFullYear();
-        const month = takenTime.getMonth();
+        const takenTime = await getPhotoTakenTime(sourceFile);
 
-        const pathParse = path.parse(file);
+        const year = padStart(takenTime.getFullYear(), 4);
+        const month = padStart(takenTime.getMonth(), 2);
+        const dayOfMonth = padStart(takenTime.getDate(), 3);
+        const hours = padStart(takenTime.getHours(), 4);
+        const minutes = padStart(takenTime.getMinutes(), 2);
 
-        const yearString = year.toString();
-        const monthString = twoDigitPad(month);
+        // IMG 2018-11-06 09.59.jpeg
+        const name = `IMG ${year}-${month}-${dayOfMonth} ${hours}-${minutes}`;
 
-        return `${organizedDir}/${yearString}/${monthString}/${pathParse.base}`
+        const pathParse = path.parse(sourceFile);
+
+        let destFile = path.format({ dir: organizedDir, name: name, ext: pathParse.ext });
+
+        if (sourceFile === destFile) {
+            return destFile;
+        }
+
+        if (await fs.pathExists(destFile)) {
+            if (getPhotoContentHash(sourceFile) === getPhotoContentHash(destFile)) {
+
+            }
+        }
     }
 
-    const organizeDir = async (dir: string) : Promise<void> => {
+    const organizeDir = async (dir: string): Promise<void> => {
         const paths = await fs.readdir(dir);
         paths.forEach(async p => {
-            const fullPath = path.format({ dir: dir, base: p });
-            const stats = await fs.stat(fullPath);
-            if (stats.isFile()) {
-                console.log("From: " + fullPath + " To: " + await getPhotoDestDir(fullPath, stats));
+
+            const sourceFile = path.format({ dir: dir, base: p });
+            if (isPhotoFile(p)) {
+                console.log("From: " + sourceFile + " To: " + await getPhotoDestFile(sourceFile));
             }
-            else if (stats.isDirectory()) {
-                return organizeDir(fullPath);
+            else {
+                const stats = await fs.stat(sourceFile);
+                if (stats.isDirectory()) {
+                    return organizeDir(sourceFile);
+                }
             }
         });
     }
