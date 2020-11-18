5b86bf85354aff7bf452c2aa8f3e0ec1c5ed3975
GeoffCox <geoff.cox@live.com>
2020-11-17 16:54:46 -0800

    Improvements

--- v3/src/main.rs
+++ v3/src/main.rs
@@ -1,24 +1,21 @@
-use chrono::Utc;
-use chrono::TimeZone;
-use std::{ffi::OsStr, fs, io, path::PathBuf, path::Path};
-use std::fs::File;
-use std::fmt;
-use std::io::prelude::*;
-use serde::*;
-use exif::{In, Tag};
 use anyhow::*;
+use chrono::TimeZone;
+use chrono::Utc;
+use exif::{In, Tag};
+use serde::*;
+use std::fmt;
+use std::fs::File;
+use std::io::prelude::*;
+use std::{ffi::OsStr, fs, io, path::Path, path::PathBuf};
 
 // ---------------------------------------- Config ---------------------------------------- //
 
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
 struct Config {
     directories: ConfigDirectories,
-
 }
 
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
 struct ConfigDirectories {
     unorganized: String,
     organized: String,
@@ -38,122 +35,130 @@ fn load_config() -> io::Result<String> {
 
 // ---------------------------------------- exif helpers ---------------------------------------- //
 
-
-
-
 // ---------------------------------------- PhotoInfo ---------------------------------------- //
 
 #[derive(Debug)]
 struct PhotoInfo {
-    path : PathBuf,
+    path: PathBuf,
     taken_date_time: chrono::DateTime<chrono::Utc>,
-    length: u64
-    // created_date_time: Option<String>,
-    // modified_date_time: Option<String>,
-    // file_hash: Option<String>,
-    // length: Option<u64>,
+    length: u64, // created_date_time: Option<String>,
+                 // modified_date_time: Option<String>,
+                 // file_hash: Option<String>,
+                 // length: Option<u64>
 }
 
-fn convert_exif_to_chrono_date_time(exif_date_time : &exif::DateTime) -> chrono::DateTime<chrono::Utc> {
+fn convert_exif_to_chrono_date_time(
+    exif_date_time: &exif::DateTime,
+) -> chrono::DateTime<chrono::Utc> {
     let date = chrono::Utc.ymd(
         exif_date_time.year as i32,
         exif_date_time.month as u32,
-        exif_date_time.day as u32);
+        exif_date_time.day as u32,
+    );
 
     let date_time = date.and_hms_nano(
         exif_date_time.hour as u32,
         exif_date_time.minute as u32,
         exif_date_time.second as u32,
-        exif_date_time.nanosecond.unwrap_or(0)
+        exif_date_time.nanosecond.unwrap_or(0),
     );
 
     match exif_date_time.offset {
         Some(offset_minutes) => date_time + chrono::Duration::minutes(offset_minutes as i64),
-        None => date_time
+        None => date_time,
     }
 }
 
-fn convert_exif_value_to_date_time(value : &exif::Value) -> anyhow::Result<Option<exif::DateTime>> {
+fn convert_exif_value_to_date_time(value: &exif::Value) -> Option<exif::DateTime> {
     if let exif::Value::Ascii(lines) = value {
         if lines.len() > 0 {
-            let date_time = exif::DateTime::from_ascii(&lines[0])?;
-            return Ok(Some(date_time));
+            if let Ok(date_time) = exif::DateTime::from_ascii(&lines[0]) {
+                return Some(date_time);
+            }
         }
     }
 
-    Ok(None)
+    None
 }
 
-fn convert_exif_value_to_u32(value : &exif::Value) -> anyhow::Result<Option<u32>> {
+fn convert_exif_value_to_u32(value: &exif::Value) -> Option<u32> {
     if let exif::Value::Ascii(lines) = value {
         if lines.len() > 0 {
-            let number = std::str::from_utf8(&lines[0])?.parse()?;
-            return Ok(Some(number));
+            if let Ok(line) = std::str::from_utf8(&lines[0]) {
+                if let Ok(number) = line.parse() {
+                    return Some(number);
+                }
+            }
         }
     }
 
-    Ok(None)
+    None
 }
 
-fn convert_exif_value_to_string(value : &exif::Value) -> anyhow::Result<Option<String>> {
+fn convert_exif_value_to_string(value: &exif::Value) -> Option<String> {
     if let exif::Value::Ascii(lines) = value {
         if lines.len() > 0 {
-            let text = std::str::from_utf8(&lines[0])?.to_string();
-            return Ok(Some(text));
+            if let Ok(text) = std::str::from_utf8(&lines[0]) {
+                return Some(text.to_string());
+            }
         }
     }
 
-    Ok(None)
+    None
 }
 
-fn convert_system_time_to_chrono_date_time(value: &std::time::SystemTime) -> anyhow::Result<chrono::DateTime<Utc>> {
+fn convert_system_time_to_chrono_date_time(
+    value: &std::time::SystemTime,
+) -> anyhow::Result<chrono::DateTime<Utc>> {
     let created_duration = value.duration_since(std::time::SystemTime::UNIX_EPOCH)?;
-    Ok(chrono::Utc.timestamp(created_duration.as_secs() as i64, created_duration.subsec_nanos()))
+    Ok(chrono::Utc.timestamp(
+        created_duration.as_secs() as i64,
+        created_duration.subsec_nanos(),
+    ))
 }
 
-
-
-fn get_exif_chrono_date_time(exif: &exif::Exif, tag: Tag) -> anyhow::Result<Option<chrono::DateTime<Utc>>> {
-
+fn get_exif_chrono_date_time(exif: &exif::Exif, tag: Tag) -> Option<chrono::DateTime<Utc>> {
     if let Some(field) = exif.get_field(tag, In::PRIMARY) {
-        if let Some(exif_date_time) = convert_exif_value_to_date_time(&field.value)? {
+        if let Some(exif_date_time) = convert_exif_value_to_date_time(&field.value) {
             let chrono_date_time = convert_exif_to_chrono_date_time(&exif_date_time);
-            let result = Some(chrono_date_time);
-            return Ok(result);
+            return Some(chrono_date_time);
         }
     }
 
-    Ok(None)
+    None
 }
 
-fn get_exif_field_u32(exif: &exif::Exif, tag: Tag) -> anyhow::Result<Option<u32>> {
+fn get_exif_field_u32(exif: &exif::Exif, tag: Tag) -> Option<u32> {
     if let Some(field) = exif.get_field(tag, In::PRIMARY) {
-        return Ok(convert_exif_value_to_u32(&field.value)?);
+        return convert_exif_value_to_u32(&field.value);
     }
 
-    Ok(None)
+    None
 }
 
-
-fn get_exif_field_string(exif: &exif::Exif, tag: Tag) -> anyhow::Result<Option<String>> {
-
+fn get_exif_field_string(exif: &exif::Exif, tag: Tag) -> Option<String> {
     if let Some(field) = exif.get_field(tag, In::PRIMARY) {
-        return Ok(convert_exif_value_to_string(&field.value)?);
+        return convert_exif_value_to_string(&field.value);
     }
 
-    Ok(None)
+    None
 }
 
-// fn get_exif_datetime(exif: &exif::Exif, tag: Tag) -> Option<chrono::DateTime<chrono::Utc>> {
+fn get_exif_chrono_date_time_pair(
+    exif: &exif::Exif,
+    date_tag: Tag,
+    sub_sec_tag: Tag,
+) -> Option<chrono::DateTime<Utc>> {
+    if let Some(date_time) = get_exif_chrono_date_time(exif, date_tag) {
+        if let Some(sub_sec) = get_exif_field_u32(exif, sub_sec_tag) {
+            return Some(date_time + chrono::Duration::milliseconds(sub_sec as i64));
+        }
 
-//     match exif.get_field(tag, In::PRIMARY) {
-//         Some(field) => {
-//             let value = field.display_value().with_unit(exif).to_string();
+        return Some(date_time);
+    }
 
-//         },
-//         None => None
-//     }
-// }
+    None
+}
 
 // DateTime, DateTimeOriginal, DateTimeDigitized
 // OffsetTime, OffsetTimeOriginal, OffsetTimeDigitized
@@ -162,38 +167,47 @@ fn get_exif_field_string(exif: &exif::Exif, tag: Tag) -> anyhow::Result<Option<S
 fn get_photo_info(file_path: &Path) -> anyhow::Result<PhotoInfo> {
     let file = File::open(&file_path)?;
 
+    println!("file: {:?}", file_path);
+
     let mut bufreader = std::io::BufReader::new(&file);
     let exifreader = exif::Reader::new();
-    let exif = exifreader.read_from_container(&mut bufreader)?;
+    if let Ok(exif) = exifreader.read_from_container(&mut bufreader) {
+        // exif original
+        if let Some(date_time) =
+            get_exif_chrono_date_time_pair(&exif, Tag::DateTime, Tag::SubSecTime)
+        {
+            println!("  date_time:           {:?}", date_time);
+        }
+
+        // exif original
+        if let Some(date_time_original) =
+            get_exif_chrono_date_time_pair(&exif, Tag::DateTimeOriginal, Tag::SubSecTimeOriginal)
+        {
+            println!("  date_time_original:  {:?}", date_time_original);
+        }
+
+        // exif digitized
+        if let Some(date_time_digitized) =
+            get_exif_chrono_date_time_pair(&exif, Tag::DateTimeDigitized, Tag::SubSecTimeDigitized)
+        {
+            println!("  date_time_digitized: {:?}", date_time_digitized);
+        }
+    }
 
     let metadata = fs::metadata(file_path)?;
 
-
-
     // file created
     let created_date_time = convert_system_time_to_chrono_date_time(&metadata.created()?)?;
-    println!("created_date_time: {:?}", created_date_time);
+    println!("  created_date_time:   {:?}", created_date_time);
 
     // file modified
     let modified_date_time = convert_system_time_to_chrono_date_time(&metadata.modified()?)?;
-    println!("modified_date_time: {:?}", modified_date_time);
-
-    // exif original
-    let date_time_original = get_exif_chrono_date_time(&exif, Tag::DateTimeOriginal)?.unwrap();
-    println!("date_time_original {:?}", date_time_original);
-
-    let subsec_time_original = get_exif_field_u32(&exif, Tag::SubSecTimeOriginal)?.unwrap();
-    println!("subsec_time_original {:?}", subsec_time_original);
-
-    // exif digitized
-    //let date_time_digitized = get_exif_field_string_value(&exif, Tag::DateTimeDigitized)?;
-    //let subsec_time_digitized = get_exif_field_string_value(&exif, Tag::SubSecTimeDigitized)?;
-    //println!("date_time_digitized: {:?} - {:?}", date_time_digitized, subsec_time_digitized);
+    println!("  modified_date_time:  {:?}", modified_date_time);
 
     let photo_info = PhotoInfo {
         path: PathBuf::from(&file_path),
-        taken_date_time: date_time_original,
-        length: metadata.len()
+        taken_date_time: Utc::now(),
+        length: metadata.len(),
     };
 
     return Ok(photo_info);
@@ -205,27 +219,40 @@ fn print_all_exif(file_path: &Path) -> anyhow::Result<()> {
     let exif_reader = exif::Reader::new();
     let exif = exif_reader.read_from_container(&mut buf_reader)?;
     for f in exif.fields() {
-        println!("{} {} {}",
-                 f.tag, f.ifd_num, f.display_value().with_unit(&exif));
+        println!(
+            "{} {} {}",
+            f.tag,
+            f.ifd_num,
+            f.display_value().with_unit(&exif)
+        );
     }
 
     Ok(())
 }
 
 fn is_photo_file(path: &Path) -> bool {
-
-    let photo_extensions = [OsStr::new("jpg"), OsStr::new("jpeg"), OsStr::new("tiff")];
-
-    match path.extension() {
-        Some(extension) => path.exists() && path.is_file() && photo_extensions.contains(&extension),
-        None => false
+    if path.exists() && path.is_file() {
+        if let Some(ext) = path.extension() {
+            if let Some(str_ext) = ext.to_str() {
+                match str_ext.to_lowercase().as_str() {
+                    "jpg" => return true,
+                    "jpeg" => return true,
+                    "tiff" => return true,
+                    _ => return false,
+                }
+            }
+        }
     }
+
+    false
 }
 
 // ---------------------------------------- Directories ---------------------------------------- //
 
-fn get_photo_destination_directory(photo_info: &PhotoInfo, config: &Config) -> anyhow::Result<PathBuf> {
-
+fn get_photo_destination_directory(
+    photo_info: &PhotoInfo,
+    config: &Config,
+) -> anyhow::Result<PathBuf> {
     println!("{:?}", photo_info.taken_date_time);
     let file_path = PathBuf::from(&config.directories.unorganized);
     //file_path.push(photo_info.taken_date_time.year().to_string());
@@ -234,21 +261,19 @@ fn get_photo_destination_directory(photo_info: &PhotoInfo, config: &Config) -> a
 }
 
 fn organize_photo_file(file_path: &Path, config: &Config) -> anyhow::Result<()> {
-
     //print_all_exif(file_path)?;
 
     let photo_info = get_photo_info(file_path)?;
-    print!("{:?}", photo_info);
+    //print!("{:?}", photo_info);
     //println!();
 
     //let dest_path = get_photo_destination_directory(&photo_info, config)?;
-   // println!("{:?}", dest_path);
+    // println!("{:?}", dest_path);
 
     Ok(())
 }
 
 fn organize_directory(dir_path: &Path, config: &Config) -> anyhow::Result<()> {
-
     // file entries immediately in this directory
     let mut entries = fs::read_dir(&dir_path)?
         .map(|res| res.map(|e| e.path()))
@@ -316,4 +341,3 @@ fn main() -> anyhow::Result<()> {
 // fn is_same_photo() {}
 // fn get_photo_destination_directory() {}
 // fn get_photo_destination_filename() {}
-
